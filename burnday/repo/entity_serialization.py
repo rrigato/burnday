216c59016c89fd0bdb5a9908427b93c80c0f6f1e
Ryan Rigato <ryanrigato@gmail.com>
2021-12-21 11:28:08 -0800

    creating BurnStatus entities

---
+++ burnday/repo/entity_serialization.py
@@ -0,0 +1,43 @@
+from burnday.entities.entity_model import BurnStatus
+
+import logging
+
+
+def create_burn_status(burn_day, burn_status, county_name):
+    """Creates a BurnStatus entity
+    
+        Parameters
+        ----------
+        burn_day: datetime.date
+            date the burn status applies to
+
+        burn_status: str
+            description of whether burning is allowed for the location
+
+        county_name: str
+            name of the county to return burn status of
+
+        Returns
+        -------
+        burn_status_entity: BurnStatus
+            None if any unexpected error occurs
+
+        entity_creation_error: None
+            str if any unexpected error occurred
+        
+    """
+    burn_status_entity = BurnStatus()
+    entity_creation_error = None
+    try:
+        burn_status_entity.burn_day = burn_day
+        burn_status_entity.burn_status = burn_status
+        burn_status_entity.county_name = county_name
+        
+        logging.info("create_burn_status - BurnStatus entity successfully created")
+
+    except Exception as error_suppression:
+        logging.exception("create_burn_status - unable to create entity")
+        burn_status_entity = None
+        entity_creation_error = str(error_suppression)
+
+    return(burn_status_entity, entity_creation_error)
---
+++ tests/test_repo/test_entity_serialization.py
@@ -0,0 +1,47 @@
+from copy import deepcopy
+
+import json
+import unittest
+
+class TestEntitySerialization(unittest.TestCase):
+
+    def test_create_burn_status(self):
+        """Happy Path entity created"""
+        from burnday.entities.entity_model import BurnStatus
+        from burnday.repo.entity_serialization import create_burn_status
+        from datetime import date
+
+        mock_burn_day = date.fromisoformat("3005-11-29")
+        mock_burn_status = "No Burning Unless Registered"
+        mock_county_name = "Sequoia National Park and Forest"
+
+
+        burn_status_entity, entity_creation_error = create_burn_status(
+            burn_day=mock_burn_day,
+            burn_status=mock_burn_status,
+            county_name=mock_county_name
+        )
+
+
+        self.assertIsInstance(burn_status_entity, BurnStatus)
+        self.assertIsNone(entity_creation_error)
+
+
+    def test_create_burn_status_invalid_input(self):
+        """Unhappy Path entity passed a str instead of datetime.date for entity creation"""
+        from burnday.repo.entity_serialization import create_burn_status
+
+        mock_burn_day = "3005-11-29"
+        mock_burn_status = "No Burning Unless Registered"
+        mock_county_name = "Sequoia National Park and Forest"
+
+
+        burn_status_entity, entity_creation_error = create_burn_status(
+            burn_day=mock_burn_day,
+            burn_status=mock_burn_status,
+            county_name=mock_county_name
+        )
+
+
+        self.assertEqual(type(entity_creation_error), str)
+        self.assertIsNone(burn_status_entity)
