7a4322b23cd3f308b1b4f9674dafe25b39922a5d
Ryan Rigato <ryanrigato@gmail.com>
2021-12-21 07:13:41 -0800

    stub out the burnday skill

--- burnday/repo/burn_status_storage.py
+++ burnday/repo/burn_status_storage.py
@@ -15,4 +15,23 @@ def burn_status_for_county(county_name):
         str_validation_error: None
             str if any unexpected error occurred when loading 
     """
+    '''
+        TODO - load dynamoDB table persistant storage
+        PK - county_name
+        SK - Date
+        burn_status - str
+
+        from boto3.dynamodb import conditions
+        import boto3
+        dynamodb_table = boto3.resource(
+            service_name="dynamodb",
+            region_name=os.environ.get("AWS_REGION")
+        ).Table(table_name)
+
+        existing_items = dynamo_table.query(
+            KeyConditionExpression=(
+                conditions.Key("PK").eq("burnday#" + county_name)
+            )
+        )
+    '''
     pass
---
+++ tests/test_handlers/test_burnday_skill.py
@@ -0,0 +1,17 @@
+from copy import deepcopy
+
+import json
+import unittest
+
+class TestBurndaySkill(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        with open("tests/events/burnday_skill_invoke.json", "r") as lambda_event:
+            cls.lambda_event = json.load(lambda_event)
+
+    def test_burnday_get(self):
+        """TODO - implementation based on alexa skills kit"""
+        from burnday.handlers.burnday_skill import lambda_handler
+
+
