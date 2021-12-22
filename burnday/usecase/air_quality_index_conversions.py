aff52445653ad8daef190ad1c69cf037f8404a62
Ryan Rigato <ryanrigato@gmail.com>
2021-12-22 14:48:26 -0800

    public contract for usecase

--- burnday/usecase/air_quality_index_conversions.py
+++ burnday/usecase/air_quality_index_conversions.py
@@ -1,10 +1,25 @@
 def aqi_to_pm_2point5(populated_burn_status):
-    """Uses air_quality_index attribute to calculate and mutate the 
-        fine_particulate_matter_2_5 attribute for a BurnStatus entity
+    """Uses populated_burn_status.air_quality_index to calculate and mutate the 
+        populated_burn_status.fine_particulate_matter_2_5 attribute
 
         Parameters
         ----------
         populated_burn_status: BurnStatus
-            if populated_burn_status.air_quality_index is None, no mutation occurs
+            if populated_burn_status.air_quality_index is None, 
+            no attributes are modified
+    """
+    pass
+
+
+
+def aqi_to_pm_10(populated_burn_status):
+    """Uses populated_burn_status.air_quality_index attribute to calculate and mutate the 
+        populated_burn_status.coarse_particulate_matter_10 attribute
+
+        Parameters
+        ----------
+        populated_burn_status: BurnStatus
+            if populated_burn_status.air_quality_index is None, 
+            no attributes are modified
     """
     pass
--- tests/test_usecase/test_air_quality_index_conversions.py
+++ tests/test_usecase/test_air_quality_index_conversions.py
@@ -5,6 +5,15 @@ class TestAirQualityIndexConversions(unittest.TestCase):
         """BurnStatus.fine_particulate_matter_2_5 attribute updated"""
         pass
 
-    def test_aqi_to_pm_2point5(self):
-        """pathBurnStatus.air_quality_index is None no mutation occurred"""
+    def test_aqi_to_pm_2point5_air_quality_index_is_none(self):
+        """air_quality_index attr is None, no mutation occurred on fine_particulate_matter_2_5"""
+        pass
+
+
+    def test_aqi_to_pm_10(self):
+        """BurnStatus.fine_particulate_matter_2_5 attribute updated"""
+        pass
+
+    def test_aqi_to_pm_10_air_quality_index_is_none(self):
+        """air_quality_index attr is None, no mutation occurred on coarse_particulate_matter_10"""
         pass
