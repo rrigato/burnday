59756083f3e118299d808691ec836e7dc9315fd0
Ryan Rigato <ryanrigato@gmail.com>
2021-12-23 07:17:24 -0800

    aqi lower and upper bounds

--- burnday/entities/air_quality_index_mapper.py
+++ burnday/entities/air_quality_index_mapper.py
@@ -11,17 +11,20 @@ def _apply_good_aqi_category(aqi_breakpoints):
             {
                 "aqi_category": str,
                 "aqi_color": str,
+                "aqi_lower": float,
+                "aqi_upper": float,
                 "pm_2_5_lower": float,
                 "pm_2_5_upper": float,
                 "pm_10_lower": float,
                 "pm_10_upper": float
-
             }
     """
     for air_quality_value in range(0, 51):
         aqi_breakpoints[air_quality_value] = {
             "aqi_category": "good",
             "aqi_color": "green",
+            "aqi_lower": 0,
+            "aqi_upper": 50,
             "pm_2_5_lower": 0.0,
             "pm_2_5_upper": 12.0,
             "pm_10_lower": 0.0,
@@ -29,7 +32,36 @@ def _apply_good_aqi_category(aqi_breakpoints):
         }
 
 
+def _apply_moderate_aqi_category(aqi_breakpoints):
+    """Populates dict keys with the air quality index values in the moderate range
 
+        Parameters
+        -------
+        aqi_breakpoints: dict
+            key is an int representing the air quality index, value is a dict with the
+            following structure:
+            {
+                "aqi_category": str,
+                "aqi_color": str,
+                "aqi_lower": float,
+                "aqi_upper": float,
+                "pm_2_5_lower": float,
+                "pm_2_5_upper": float,
+                "pm_10_lower": float,
+                "pm_10_upper": float
+            }
+    """
+    for air_quality_value in range(0, 51):
+        aqi_breakpoints[air_quality_value] = {
+            "aqi_category": "moderate",
+            "aqi_color": "green",
+            "aqi_lower": 0,
+            "aqi_upper": 50,
+            "pm_2_5_lower": 0.0,
+            "pm_2_5_upper": 12.0,
+            "pm_10_lower": 0.0,
+            "pm_10_upper": 54.0 
+        }
 
 def aqi_to_pm_breakpoints():
     """Static mapping table between the air quality index and the fine particulate matter (PM2.5)
@@ -44,16 +76,20 @@ def aqi_to_pm_breakpoints():
             {
                 "aqi_category": str,
                 "aqi_color": str,
+                "aqi_lower": float,
+                "aqi_upper": float,
                 "pm_2_5_lower": float,
                 "pm_2_5_upper": float,
                 "pm_10_lower": float,
                 "pm_10_upper": float
-
             }
-            aqi_category = "good", "MODERATE", "UNHEALTHY FOR SENSITIVE", "UNHEALTHY", 
+            aqi_category = "good", "moderate", "UNHEALTHY FOR SENSITIVE", "UNHEALTHY", 
             "VERY UNHEALTHY", "HAZARDOUS"
             aqi_color = color to symbolize category
             https://www.epa.gov/outdoor-air-quality-data/air-data-basic-information
+
+            aqi_lower = air quality index lower bound for given air quality index
+            aqi_upper = air quality index upper bound for given air quality index            
 
             pm_2_5_lower = fine particulate matter lower bound for given air quality index
             pm_2_5_upper = fine particulate matter upper bound for given air quality index
@@ -64,5 +100,7 @@ def aqi_to_pm_breakpoints():
     aqi_mapping_table = {}
 
     _apply_good_aqi_category(aqi_breakpoints=aqi_mapping_table)
+
+    # _apply_moderate_aqi_category(aqi_breakpoints=aqi_mapping_table)
 
     return(deepcopy(aqi_mapping_table))
--- tests/test_entities/test_air_quality_index_mapper.py
+++ tests/test_entities/test_air_quality_index_mapper.py
@@ -3,7 +3,7 @@ import unittest
 class TestAirQualityIndexMapper(unittest.TestCase):
 
     def test_aqi_to_pm_breakpoints(self):
-        """Ensures data structure of table breakpoints"""
+        """Ensures data structure of air quality index breakpoints"""
         from burnday.entities.air_quality_index_mapper import aqi_to_pm_breakpoints
 
         aqi_breakpoints = aqi_to_pm_breakpoints()
@@ -22,6 +22,16 @@ class TestAirQualityIndexMapper(unittest.TestCase):
                 self.assertEqual(
                     type(aqi_breakpoints[possible_aqi_value]["aqi_color"]),
                     str
+                )
+
+                self.assertEqual(
+                    type(aqi_breakpoints[possible_aqi_value]["aqi_lower"]),
+                    int
+                )
+
+                self.assertEqual(
+                    type(aqi_breakpoints[possible_aqi_value]["aqi_upper"]),
+                    int
                 )
 
                 self.assertEqual(
