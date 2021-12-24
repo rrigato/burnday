69d50811df224be910f6d0eef9e12511e3e9f5fc
Ryan Rigato <ryanrigato@gmail.com>
2021-12-24 07:48:06 -0800

    psuedo code and test case

--- burnday/usecase/air_quality_index_conversions.py
+++ burnday/usecase/air_quality_index_conversions.py
@@ -1,3 +1,16 @@
+from burnday.entities.air_quality_index_mapper import aqi_to_pm_breakpoints
+def _aqi_conversion_formula(aqi_value, metric_lower_bound_name, metric_upper_bound_name):
+    """"""
+    '''
+        TODO - 
+        aqi_breakpoints = aqi_to_pm_breakpoints()
+        pm = 
+        (aqi*(metric_upper_breakpoint - metric_lower_breakpoint) + aqi_upper*metric_lower - aqi_lower*metric_lower)
+        / (aqi_upper - aqi_lower)
+        
+    '''
+    pass
+
 def aqi_to_pm_2point5(populated_burn_status):
     """Uses populated_burn_status.air_quality_index to calculate and mutate the 
         populated_burn_status.fine_particulate_matter_2_5 attribute
@@ -8,7 +21,17 @@ def aqi_to_pm_2point5(populated_burn_status):
             if populated_burn_status.air_quality_index is None, 
             no attributes are modified
     """
-    pass
+    if populated_burn_status.air_quality_index is None:
+        return(None)
+    '''
+        TODO-
+        _aqi_conversion_formula(
+            aqi_value=populated_burn_status.air_quality_index,
+            metric_lower_bound_name="pm_2_5_lower",
+            metric_upper_bound_name="pm_2_5_upper"
+        )
+    '''   
+
 
 
 
--- tests/test_usecase/test_air_quality_index_conversions.py
+++ tests/test_usecase/test_air_quality_index_conversions.py
@@ -7,7 +7,29 @@ class TestAirQualityIndexConversions(unittest.TestCase):
 
     def test_aqi_to_pm_2point5_air_quality_index_is_none(self):
         """air_quality_index attr is None, no mutation occurred on fine_particulate_matter_2_5"""
-        pass
+        from burnday.entities.entity_model import BurnStatus
+        from burnday.usecase.air_quality_index_conversions import aqi_to_pm_2point5
+        from datetime import date
+
+        mock_burn_day = "3005-11-29"
+        mock_zip_code = 20002
+        mock_air_quality_index = None
+
+        burn_status_entity = BurnStatus()
+
+        burn_status_entity.burn_day = date.fromisoformat(mock_burn_day)
+        burn_status_entity.zip_code = mock_zip_code
+        burn_status_entity.air_quality_index = mock_air_quality_index
+
+
+        aqi_to_pm_2point5(populated_burn_status=burn_status_entity)
+
+
+        self.assertEqual(burn_status_entity.burn_day.isoformat(), mock_burn_day)
+        self.assertEqual(burn_status_entity.zip_code, mock_zip_code)
+        self.assertEqual(burn_status_entity.air_quality_index, mock_air_quality_index)
+        self.assertIsNone(burn_status_entity.air_quality_index)
+        self.assertIsNone(burn_status_entity.fine_particulate_matter_2_5)
 
 
     def test_aqi_to_pm_10(self):
