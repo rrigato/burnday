b0c0c03d65cc94b374a34fab128e8a30f8de2a15
Ryan Rigato <ryanrigato@gmail.com>
2021-12-25 13:21:53 -0800

    default ruleset

--- burnday/usecase/zip_code_burn_evaluation_logic.py
+++ burnday/usecase/zip_code_burn_evaluation_logic.py
@@ -1,12 +1,14 @@
-
 import logging
+
+
 def default_burn_rules(populated_burn_status):
-    """Provides a generic burn message based on air quality status
+    """Mutates populated_burn_status.burn_status with generic air quality information
+
         Parameters
         ----------
         populated_burn_status: BurnStatus
-            if populated_burn_status.air_quality_index is None, 
-            no attributes are modified
+            burn_status attribute will be mutated, fine_particulate_matter_2_5 must be populated 
+
     """
     logging.info("default_burn_rules - air quality message")
     generic_air_quality_message = "air quality status is in the {aqi_range} range"
@@ -41,3 +43,40 @@ def default_burn_rules(populated_burn_status):
             aqi_range="hazardous"
         )
 
+def _get_california_valley_default_burn_rules(fine_particulate_matter_2_5):
+    pass
+
+def california_valley_default_burn_rules(populated_burn_status):
+    """Mutates populated_burn_status.burn_status with default burn status regulations 
+        based on rule 4901: 
+
+        https://valleyair.org/rule4901/documents/FAQ-Rule4901.pdf
+        https://www.valleyair.org/rules/currntrules/r4901.pdf
+
+        Parameters
+        ----------
+        populated_burn_status: BurnStatus
+            burn_status attribute will be mutated, fine_particulate_matter_2_5 must be populated 
+    """
+    populated_burn_status.fine_particulate_matter_2_5 =_get_california_valley_default_burn_rules(
+
+    )
+
+
+def california_valley_hot_spot_burn_rules(populated_burn_status):
+    """Mutates populated_burn_status.burn_status with hot spot specific burn status regulations 
+        based on rule 4901: 
+
+        https://valleyair.org/rule4901/documents/FAQ-Rule4901.pdf
+        https://www.valleyair.org/rules/currntrules/r4901.pdf
+
+        Parameters
+        ----------
+        populated_burn_status: BurnStatus
+            if populated_burn_status.air_quality_index is None, 
+            no attributes are modified
+    """
+    '''
+    TODO - apply coarse_particulate_matter_10
+    '''
+    pass
--- tests/test_usecase/test_zip_code_burn_evaluation_logic.py
+++ tests/test_usecase/test_zip_code_burn_evaluation_logic.py
@@ -30,7 +30,35 @@ class TestZipCodeBurnEvaluationLogic(unittest.TestCase):
                 )
 
 
-    def test_california_valley_burn_rules(self):
-        """California valley zip code based ruleset"""
+    def test_california_valley_default_burn_rules(self):
+        """California valley default burn ruleset"""
+        from burnday.entities.entity_model import BurnStatus
+        from burnday.usecase.zip_code_burn_evaluation_logic import california_valley_default_burn_rules
+
+
+        pm_2_5_values = [
+            {"pm_2_5_input": 0.5, "expected_burn_status": "burning discouraged"},
+            {"pm_2_5_input": 17.0, "expected_burn_status": "burning discouraged"},
+            {"pm_2_5_input": 20.1, "expected_burn_status": "no burning unless registered"},
+            {"pm_2_5_input": 64.4, "expected_burn_status": "no burning unless registered"},
+            {"pm_2_5_input": 65.2,  "expected_burn_status": "no burning for all"},
+            {"pm_2_5_input": 79.7,  "expected_burn_status": "no burning for all"}
+        ]
+
+        for pm_2_5_value in pm_2_5_values:
+            with self.subTest(pm_2_5_value=pm_2_5_value):
+                burn_status_entity = BurnStatus()
+                burn_status_entity.fine_particulate_matter_2_5 = pm_2_5_value["pm_2_5_input"]
+                
+                california_valley_default_burn_rules(populated_burn_status=burn_status_entity)
+
+                self.assertEqual(
+                    burn_status_entity.burn_status, 
+                    pm_2_5_value["expected_burn_status"]
+                )
+
+
+    def test_california_valley_hot_spot_burn_rules(self):
+        """California valley hot spot ruleset"""
         pass
 
