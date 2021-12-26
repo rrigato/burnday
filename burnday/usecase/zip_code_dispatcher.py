87e545bed0adf28a024b0ddd6c07f1c722256dd7
Ryan Rigato <ryanrigato@gmail.com>
2021-12-26 06:11:24 -0800

    map a zip code to a custom routing function

--- burnday/usecase/zip_code_dispatcher.py
+++ burnday/usecase/zip_code_dispatcher.py
@@ -19,6 +19,57 @@ def _zip_based_mapping():
     return(zip_code_router)
     
 
+def _apply_california_valley_default_burn_rules(dispatch_functions):
+    """Zip codes that use the california_valley_default_burn_rules ruleset
+    
+        Parameters
+        -------
+        dispatch_functions: dict
+            Where each key is the int zip_code for the burn status request and 
+            each value is the factory function to execute the appropriate business logic
+    """
+    '''
+        TODO - extract into seperate module if zip codes are still used?
+    '''
+    tulare_county= [
+        93201,
+        93603,
+        93208,
+        93615,
+        93618,
+        93218,
+        93219,
+        93221,
+        93223,
+        93235,
+        93633,
+        93244,
+        93247,
+        93647,
+        93207,
+        93256,
+        93258,
+        93257,
+        93260,
+        93261,
+        93262,
+        93265,
+        93267,
+        93666,
+        93270,
+        93271,
+        93272,
+        93673,
+        93274,
+        93277,
+        93291,
+        93292,
+        93286
+    ]
+    for tulare_zip_code in tulare_county:
+        dispatch_functions[tulare_zip_code] = zip_code_burn_evaluation_logic.california_valley_default_burn_rules
+
+
 def factory_router(populated_burn_status):
     """Mutates populated_burn_status.burn_status with applicable business logic
     
@@ -30,9 +81,9 @@ def factory_router(populated_burn_status):
     """
     dispatch_functions = _zip_based_mapping()
 
+    _apply_california_valley_default_burn_rules(dispatch_functions=dispatch_functions)
     '''
-        mapping zip codes for these rulesets:
-            california_valley_default_burn_rules
+        mapping zip codes for this ruleset:
             california_valley_hot_spot_burn_rules
     '''
 
--- tests/test_usecase/test_zip_code_dispatcher.py
+++ tests/test_usecase/test_zip_code_dispatcher.py
@@ -21,21 +21,35 @@ class TestZipCodeDispatcher(unittest.TestCase):
         self.assertEqual(len(dispatch_functions.keys()), 100000)
 
 
+    '''
+        TODO - 
+        patch _apply_california_valley_default_burn_rules or
+        test output dict directly?
+    '''
     @patch("burnday.usecase.zip_code_dispatcher._zip_based_mapping")
     def test_factory_router(self, mock_zip_based_mapping):
         """selects key of _zip_based_mapping and invokes corresponding function with entity"""
-        from burnday.usecase.zip_code_dispatcher import factory_router
         from burnday.entities.entity_model import BurnStatus
+        from burnday.usecase.zip_code_burn_evaluation_logic import california_valley_default_burn_rules
+        from burnday.usecase.zip_code_dispatcher import factory_router
+
 
         mock_business_rule_function = MagicMock()
         mock_burn_status = BurnStatus()
         mock_zip_code = 20002
-
-        mock_zip_based_mapping.return_value = {20002: mock_business_rule_function}
+        mock_dispatch_router = {20002: mock_business_rule_function}
+        mock_zip_based_mapping.return_value = mock_dispatch_router
         mock_burn_status.zip_code = mock_zip_code
+
 
         factory_router(populated_burn_status=mock_burn_status)
 
 
         mock_business_rule_function.assert_called_once_with(populated_burn_status=mock_burn_status)
-
+        '''
+            TODO - 
+            as-is = test random zip code has the appropriate handler
+            patch _apply_california_valley_default_burn_rules or
+            test output dict directly?
+        '''
+        self.assertEqual(mock_dispatch_router[93261], california_valley_default_burn_rules)
