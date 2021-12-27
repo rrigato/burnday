55ad8384d3bfc6cd2988e9f64eef68ba25131ec5
Ryan Rigato <ryanrigato@gmail.com>
2021-12-27 06:29:55 -0800

    happy path stub

--- burnday/repo/persistant_storage.py
+++ burnday/repo/persistant_storage.py
@@ -1,12 +1,27 @@
-def get_burnday_api_key():
-    """Loads api key for invoking airnowapi
+import boto3
+import os
+
+def get_burnday_secrets():
+    """Loads burnday project secrets and configuration from persistant storage
 
         Returns
         -------
-        burnday_api_key: str
-            None if unexpected error occurs
+        burnday_project_config: dict
+            burnday project secrets and config with the following keys:
+            {
+                "airnow_api_key": str,
+                "aqs_email": str,
+                "aqs_key": str
+            }
 
         repo_retrieval_error: None
             str if any unexpected error occurred when retrieving the api key
     """
-    pass
+    ssm_client = boto3.client("ssm", region_name=os.environ.get("AWS_REGION"))
+
+    return({}, None)
+
+
+if __name__ == "__main__":
+    os.environ["AWS_REGION"] = "us-east-1"
+    get_burnday_secrets()
--- tests/test_repo/test_persistant_storage.py
+++ tests/test_repo/test_persistant_storage.py
@@ -1,10 +1,42 @@
+from copy import deepcopy
+from unittest.mock import patch
+
+import json
 import unittest
 
 class TestPersistantStorage(unittest.TestCase):
-    def test_get_burnday_api_key(self):
-        """Happy Path, api key returned"""
-        pass
 
-    def test_get_burnday_api_key_unexpected_error(self):
+    @classmethod
+    def setUpClass(cls):
+        cls.mock_project_secrets = {
+            "mock_key1": "mock_secret0",
+            "mock_key2": "mock_secret1",
+            "mock_key3": "mock_secret2"
+        }
+        cls.MOCK_AWS_REGION = "us-east-1"
+
+    @patch("os.environ.get")
+    @patch("boto3.client")
+    def test_get_burnday_secrets(self, mock_boto3_client, mock_os_environ_get):
+        """Happy Path, api secrets and config returned"""
+        from burnday.repo.persistant_storage import get_burnday_secrets
+
+        mock_boto3_client.return_value.get_parameter.return_value = deepcopy(
+            {
+                "Parameter":{
+                    "Value": json.dumps(deepcopy(self.mock_project_secrets))
+                }
+            }
+        )
+        mock_os_environ_get.return_value = self.MOCK_AWS_REGION
+
+
+        burnday_project_config, config_retrieval_error = get_burnday_secrets()
+
+
+        mock_boto3_client.assert_called_once_with("ssm", region_name="us-east-1")
+        self.assertIsNone(config_retrieval_error)
+
+    def test_get_burnday_secrets_unexpected_error(self):
         """Unhappy Path, unable to load api keys on unexpected error"""
         pass
