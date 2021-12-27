9389764d0af26457f82ad595e58b2e2defbf6e07
Ryan Rigato <ryanrigato@gmail.com>
2021-12-27 11:42:18 -0800

    last modified time

--- devops/lambda_deployment.ps1
+++ devops/lambda_deployment.ps1
@@ -14,13 +14,10 @@ if(-not ( (detect-secrets scan | ConvertFrom-Json).results.ToString() -eq "") ){
 }
 
 
-#TODO - get rid of __pycache__ files
-#add tags to uploaded bucket
-#Switch to ${bundle_dir_name} archive in cloudformation template
-#perform lambda function code update
+#TODO - get rid of __pycache__ files locally
+
 
 if (Test-Path -Path "${bundle_dir_name}"){
-    echo "Removing ${bundle_dir_name} directory"
     #remove ${bundle_dir_name} contents recursively
     Get-ChildItem -Path "${bundle_dir_name}" -Recurse | Remove-Item -force -recurse
     #remove ${bundle_dir_name} directory
@@ -29,6 +26,7 @@ if (Test-Path -Path "${bundle_dir_name}"){
     echo "${bundle_dir_name} directory not found"
 }
 
+#excludes lambda files
 $exclude_files = (Get-ChildItem -Path "./${project_name}/*/__pycache__" -Recurse ).FullName
 
 
@@ -39,7 +37,17 @@ Copy-Item -Path  "handlers/${project_name}_skill.py"  -Force -Destination "${bun
 
 Compress-Archive -Path "${bundle_dir_name}\*" -DestinationPath  "${project_name}_deployment_package.zip" -Force
 
-aws s3api put-object --bucket "${project_name}-app-artifacts" --key "${project_name}_deployment_package.zip" --body "${project_name}_deployment_package.zip" --tagging "cloudformation_managed=no&project=${project_name}&prod=yes"
+
+if (-not (Test-Path -Path "${project_name}_deployment_package.zip")){
+    throw "zip archive unsuccessful";
+}
+
+#upload archive to s3 and do not display output
+aws s3api put-object --bucket "${project_name}-app-artifacts" --key "${project_name}_deployment_package.zip" --body "${project_name}_deployment_package.zip" --tagging "cloudformation_managed=no&project=${project_name}&prod=yes" | Out-Null
 
 
-aws lambda update-function-code --function-name "${project_name}-alexa-skill" --s3-bucket "${project_name}-app-artifacts" --s3-key "${project_name}_deployment_package.zip" | ConvertFrom-Json
+
+#update lambda function code and print the last modified time in UTC
+$lambda_modification_output = (aws lambda update-function-code --function-name "${project_name}-alexa-skill" --s3-bucket "${project_name}-app-artifacts" --s3-key "${project_name}_deployment_package.zip" | ConvertFrom-Json).LastModified.substring(0,19)
+
+echo "lambda funciton last modified - ${lambda_modification_output}"
