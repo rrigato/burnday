c14dfffe21af46ad4583e4d9afade4e4368b7311
Ryan Rigato <ryanrigato@gmail.com>
2021-12-31 12:10:23 -0800

    load api response

--- burnday/repo/burn_status_storage.py
+++ burnday/repo/burn_status_storage.py
@@ -1,8 +1,101 @@
 from burnday.repo.entity_serialization import create_burn_status
 from burnday.repo.persistant_storage import get_burnday_secrets
 from datetime import date
+from urllib.parse import urlencode
+from urllib.request import Request
+from urllib.request import urlopen
 
 import logging
+import json
+import uuid
+
+def _get_encoded_parameters(airnow_api_key, zip_code):
+    """Returns url encoded query parameters
+
+        Parameters
+        ----------
+        airnow_api_key: str
+            airnow api key for HTTP get request
+
+        zip_code: int
+            numeric postal code
+            
+        Returns
+        -------
+        url_encoded_query_string: str
+
+    """
+    return(urlencode(
+        {
+            "api_key": airnow_api_key,
+            "distance": 500,
+            "format": "json",
+            "zipCode": str(zip_code).rjust(5, '0')
+        }
+    ))
+
+def _handle_api_request(airnow_api_key, zip_code):
+    """orchestrates api request to AirNow api
+
+        Parameters
+        ----------
+        airnow_api_key: str
+            airnow api key for HTTP get request
+
+        zip_code: int
+            numeric postal code
+            
+        Returns
+        -------
+        airnow_api_response: list
+            list of dict where each dict has the following keys
+            {
+                "DateIssue": str in YYYY-MM-DD,
+                "DateForecast": str in YYYY-MM-DD,
+                "ReportingArea": str,
+                "StateCode": str ,
+                "Latitude": float,
+                "Longitude": float,
+                "ParameterName": str,
+                "AQI": int,
+                "Category": {
+                    "Number": int,
+                    "Name": str
+                },
+                "ActionDay": boolean,
+                "Discussion": str
+            } 
+            or None if unexpected error occured
+
+
+        repo_retrieval_error: None
+            str if any unexpected error occurred when retrieving the BurnStatus entity 
+
+    """
+    try:
+        full_request_url = "https://www.airnowapi.org/aq/forecast/zipCode?{query_string}".format(
+            query_string=_get_encoded_parameters(airnow_api_key=airnow_api_key, zip_code=zip_code)
+        )
+
+        request_id_header = uuid.uuid4().hex
+
+        logging.info("_handle_api_request - request_id_header - " + request_id_header)
+
+        get_request = Request(full_request_url)
+
+        get_request.add_header("x-request-id", request_id_header)
+
+        with urlopen(get_request) as api_response:
+            assert api_response.getcode() == 200, (
+                "_handle_api_request - api_response.getcode" + str(api_response.getcode())
+            )
+
+            return(json.loads(api_response.read()), None)
+
+    except Exception as error_suppression:
+        logging.exception("_handle_api_request - unexpected error")
+        return(None, str(error_suppression))
+    
 
 
 def load_burn_status(zip_code):
@@ -49,6 +142,15 @@ def load_burn_status(zip_code):
 
         return(None, config_retrieval_error)
 
+    airnow_api_response, repo_retrieval_error = _handle_api_request(
+        airnow_api_key=burnday_project_config["airnow_key"], 
+        zip_code=zip_code
+    )
+
+    if repo_retrieval_error is not None:
+        logging.info("load_burn_status - _handle_api_request error")
+        return(None, repo_retrieval_error)
+
     return(
         create_burn_status(
             burn_day=date.today(),
@@ -57,3 +159,7 @@ def load_burn_status(zip_code):
         
         )
     )
+
+
+if __name__ == "__main__":
+    load_burn_status(20002)
