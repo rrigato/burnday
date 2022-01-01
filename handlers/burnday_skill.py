0f7382fb3321ece3328fdd73744bc61ee4904152
Ryan Rigato <ryanrigato@gmail.com>
2022-01-01 07:43:05 -0800

    more sample utterances

--- handlers/burnday_skill.py
+++ handlers/burnday_skill.py
@@ -16,11 +16,6 @@ if __name__ == "__main__":
     import json
     import os
     os.environ["AWS_REGION"] = "us-east-1"
-    zip_code_request = validate_location_burn_status(zip_code=93261)
-
-    location_burn_status_response = location_burn_status(zip_code_request=zip_code_request)
-
-    logging.info(location_burn_status_response.response_value.burn_status)
 
     with open("tests/events/intent_requests/burn_status_intent.json", "r") as intent_request:
         intent_request = json.load(intent_request)
