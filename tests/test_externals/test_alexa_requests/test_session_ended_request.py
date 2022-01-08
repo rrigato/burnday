a1d6f770f924dfbdc69d4cdffef1ded0a7d2d566
Ryan Rigato <ryanrigato@gmail.com>
2022-01-08 07:21:45 -0800

    test outgoing arguements

--- tests/test_externals/test_alexa_requests/test_session_ended_request.py
+++ tests/test_externals/test_alexa_requests/test_session_ended_request.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+from unittest.mock import patch
+
 import json
 import unittest
 
@@ -8,7 +11,20 @@ class TestSessionEndedRequest(unittest.TestCase):
         with open("tests/events/intent_requests/session_ended_request.json", "r") as intent_request:
             cls.intent_request = json.load(intent_request)
 
-
-    def test_session_ended_request(self):
+    @patch("externals.alexa_intents.intent_dispatcher.SessionEndedRequestHandler.handle")
+    def test_session_ended_request(self, mock_handle):
         """If cleanup logic is ever required, write test case here"""
-        from externals.alexa_requests.session_ended_request import SessionEndedRequestHandler
+        from externals.alexa_intents.intent_dispatcher import get_alexa_lambda_handler
+
+        expected_message = "Provide a zip code to receive that location's burn status"
+
+
+        alexa_lambda_handler = get_alexa_lambda_handler()
+
+
+        actual_response_message = alexa_lambda_handler(
+            deepcopy(self.intent_request), 
+            None
+        )
+
+        self.assertEqual(mock_handle.call_count, 1)
