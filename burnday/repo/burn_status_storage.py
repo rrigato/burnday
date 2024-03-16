bdcb32c667f8694d1d7e021a1d039b6f30cfa8de
rrigato <ryanrigato@gmail.com>
2024-03-16 15:58:27 -0500

    include add and commit into deployment script

--- burnday/repo/burn_status_storage.py
+++ burnday/repo/burn_status_storage.py
@@ -19,7 +19,7 @@ def _get_encoded_parameters(airnow_key, zip_code):
 
         zip_code: int
             numeric postal code
-            
+
         Returns
         -------
         url_encoded_query_string: str
@@ -44,11 +44,11 @@ def _handle_api_request(burnday_project_config, zip_code):
                 "airnow_key": str,
                 "aqs_email": str,
                 "aqs_key": str
-            } 
+            }
 
         zip_code: int
             numeric postal code
-            
+
         Returns
         -------
         airnow_api_response: list
@@ -68,18 +68,18 @@ def _handle_api_request(burnday_project_config, zip_code):
                 },
                 "ActionDay": boolean,
                 "Discussion": str
-            } 
+            }
             or None if unexpected error occured
 
 
         repo_retrieval_error: None
-            str if any unexpected error occurred when retrieving the BurnStatus entity 
+            str if any unexpected error occurred when retrieving the BurnStatus entity
 
     """
     try:
         full_request_url = "https://www.airnowapi.org/aq/forecast/zipCode?{query_string}".format(
             query_string=_get_encoded_parameters(
-                airnow_key=burnday_project_config["airnow_key"], 
+                airnow_key=burnday_project_config["airnow_key"],
                 zip_code=zip_code
             )
         )
@@ -102,7 +102,7 @@ def _handle_api_request(burnday_project_config, zip_code):
     except Exception as error_suppression:
         logging.exception("_handle_api_request - unexpected error")
         return(None, str(error_suppression))
-    
+
 def _select_closest_forecast(airnow_api_response):
     """Returns the airnow forecast with the oldest DateForecast because the airnow api will not
     return any forecasts older than todays current date
@@ -126,7 +126,7 @@ def _select_closest_forecast(airnow_api_response):
                 },
                 "ActionDay": boolean,
                 "Discussion": str
-            } 
+            }
             or None if unexpected error occured
 
         Returns
@@ -148,8 +148,8 @@ def _select_closest_forecast(airnow_api_response):
                 },
                 "ActionDay": boolean,
                 "Discussion": str
-            } 
-            
+            }
+
     """
     oldest_airnow_forecast = airnow_api_response[0]
     if len(airnow_api_response) == 1:
@@ -185,7 +185,7 @@ def _select_todays_air_quality(airnow_api_response, zip_code):
                 },
                 "ActionDay": boolean,
                 "Discussion": str
-            } 
+            }
             or None if unexpected error occured
 
         zip_code: int
@@ -195,14 +195,14 @@ def _select_todays_air_quality(airnow_api_response, zip_code):
         -------
         burn_status_entity: BurnStatus
             None if no BurnStatus entites were found for the passed zip_code
-            Populates the following attributes: 
+            Populates the following attributes:
                 burn_status_entity.burn_day = datetime.date
                 burn_status_entity.air_quality_index = int
                 burn_status_entity.zip_code = zip_code
 
 
         repo_retrieval_error: None
-            str if any unexpected error occurred when retrieving the BurnStatus entity 
+            str if any unexpected error occurred when retrieving the BurnStatus entity
 
     """
     try:
@@ -210,17 +210,17 @@ def _select_todays_air_quality(airnow_api_response, zip_code):
             logging.warning("_select_todays_air_quality - no results returned in list")
             return(None, "No air quality index found for today")
 
-        
+
         logging.info("_select_todays_air_quality - applying _select_closest_forecast")
         airnow_api_forecast = _select_closest_forecast(airnow_api_response=airnow_api_response)
 
         logging.info("_select_todays_air_quality - invoking create_burn_status")
-        return(        
+        return(
             create_burn_status(
                 burn_day=date.fromisoformat(airnow_api_forecast["DateForecast"].strip()),
                 air_quality_index=airnow_api_forecast["AQI"],
                 zip_code=zip_code
-            
+
             )
         )
 
@@ -230,25 +230,25 @@ def _select_todays_air_quality(airnow_api_response, zip_code):
 
 
 def load_burn_status(zip_code):
-    """Retrieves a BurnStatus entity from persistant storage 
+    """Retrieves a BurnStatus entity from persistant storage
 
         Parameters
         ----------
         zip_code: int
             numeric postal code
-            
+
         Returns
         -------
         burn_status_entity: BurnStatus
             None if no BurnStatus entites were found for the passed zip_code
-            Populates the following attributes: 
+            Populates the following attributes:
                 burn_status_entity.burn_day = datetime.date
                 burn_status_entity.air_quality_index = int
                 burn_status_entity.zip_code = zip_code
 
 
         repo_retrieval_error: None
-            str if any unexpected error occurred when retrieving the BurnStatus entity 
+            str if any unexpected error occurred when retrieving the BurnStatus entity
     """
     burnday_project_config, config_retrieval_error = get_burnday_secrets()
     if config_retrieval_error is not None:
@@ -257,7 +257,7 @@ def load_burn_status(zip_code):
         return(None, config_retrieval_error)
 
     airnow_api_response, repo_retrieval_error = _handle_api_request(
-        burnday_project_config=burnday_project_config, 
+        burnday_project_config=burnday_project_config,
         zip_code=zip_code
     )
 
@@ -267,7 +267,7 @@ def load_burn_status(zip_code):
 
 
     logging.info("load_burn_status - _handle_api_request success")
- 
+
     return(
         _select_todays_air_quality(airnow_api_response=airnow_api_response, zip_code=zip_code)
     )
@@ -280,4 +280,4 @@ if __name__ == "__main__":
         print(burn_status_entity.air_quality_index)
         print(burn_status_entity.zip_code)
 
-    print(repo_error)
+    
