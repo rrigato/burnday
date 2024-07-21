000f6274f41c0bbf15395a0082352bfde4754cee
rrigato <ryanrigato@gmail.com>
2024-07-21 09:57:16 -0500

    skipping commands

--- scripts/burnday_build_test.sh
+++ scripts/burnday_build_test.sh
@@ -45,25 +45,25 @@ echo $DEPLOYMENT_PACKAGE
 
 ls -l
 
-zip -v -u $DEPLOYMENT_PACKAGE -j externals  \
-    -x *__pycache__*
+# zip -v -u $DEPLOYMENT_PACKAGE -j externals  \
+#     -x *__pycache__*
 
 echo "--------deployment package created--------"
 
 
-aws s3api put-object --bucket $BUCKET_NAME \
-    --region $REGION_NAME \
-    --key $PROJECT_NAME/$DEPLOYMENT_PACKAGE \
-    --body $DEPLOYMENT_PACKAGE \
-    --tagging "cloudformation=no&project=${PROJECT_NAME}&keep=yes"
+# aws s3api put-object --bucket $BUCKET_NAME \
+#     --region $REGION_NAME \
+#     --key $PROJECT_NAME/$DEPLOYMENT_PACKAGE \
+#     --body $DEPLOYMENT_PACKAGE \
+#     --tagging "cloudformation=no&project=${PROJECT_NAME}&keep=yes"
 
 
 
 
-aws lambda update-function-code \
-    --region $REGION_NAME \
-    --function-name  "${PROJECT_NAME}-alexa-skill" \
-    --s3-bucket $BUCKET_NAME \
-    --s3-key $PROJECT_NAME/$DEPLOYMENT_PACKAGE \
-    --no-cli-pager
+# aws lambda update-function-code \
+#     --region $REGION_NAME \
+#     --function-name  "${PROJECT_NAME}-alexa-skill" \
+#     --s3-bucket $BUCKET_NAME \
+#     --s3-key $PROJECT_NAME/$DEPLOYMENT_PACKAGE \
+#     --no-cli-pager
 
